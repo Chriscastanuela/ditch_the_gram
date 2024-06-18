4bb4184aaf5ef76067ec174fd691c2bda3ca3138
Chriscastanuela <chriscastanuela@ymail.com>
2024-06-18 05:34:43 -0600

    add authlayout conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
             {/* public */}
             <Route element={<AuthLayout/>}>
                 <Route path='sign-in' element={<SigninForm/>}/>
-                <Route path='sign-in' element={<SignupForm/>}/>
+                <Route path='sign-up' element={<SignupForm/>}/>
             </Route>
             {/* private */}
             <Route element={<RootLayout/>}>
